ec5f674789a4dcf5fd241697318b7a927e8244be
pugur <ryota52399@gmail.com>
2024-08-18 03:59:11 +0900

    autoEatTweak improved

--- src/main/java/com/pugur/kindatweaks/mixin/MixinMinecraftClient.java
+++ src/main/java/com/pugur/kindatweaks/mixin/MixinMinecraftClient.java
@@ -10,12 +10,13 @@ import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.option.GameOptions;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.item.FoodComponent;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.packet.c2s.play.UpdateSelectedSlotC2SPacket;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -25,6 +26,8 @@ public class MixinMinecraftClient {
     @Shadow @Nullable public Screen currentScreen;
     @Shadow @Final public GameOptions options;
     @Shadow @Nullable public ClientPlayerEntity player;
+    @Unique private boolean flag = false;
+    @Unique
 
     @Inject(method = "handleInputEvents", at = @At("HEAD"))
     private void onProcessKeybindsPre(CallbackInfo ci)
@@ -40,18 +43,25 @@ public class MixinMinecraftClient {
 
     @Inject(method = "tick", at = @At("HEAD"))
     private void onTick(CallbackInfo ci) {
-        if (player == null || KindaTweaksClient.mc == null) return;
-        if (KindaTweaksClient.mc.getNetworkHandler() == null || KindaTweaksClient.mc.interactionManager == null) return;tw
         if (Configs.Generic.MOD_ENABLED.getBooleanValue() && FeatureToggle.TWEAK_AUTO_EAT.getBooleanValue()) {
+            if (player == null || KindaTweaksClient.mc == null) return;
+            if (KindaTweaksClient.mc.getNetworkHandler() == null || KindaTweaksClient.mc.interactionManager == null) return;
             if (player.getHungerManager().isNotFull()) {
                 for (int i = 0; i < player.getInventory().size(); i++) {
                     ItemStack stack = player.getInventory().getStack(i);
                     if (stack.getItem().isFood()) {
                         InventoryUtil.tryToSwap(KindaTweaksClient.mc, i);
                         KeyBinding.setKeyPressed(InputUtil.fromTranslationKey(this.options.useKey.getBoundKeyTranslationKey()), true);
+                        flag = true;
                         return;
                     }
                 }
+            }
+            if (flag) {
+                KeyBinding.setKeyPressed(InputUtil.fromTranslationKey(this.options.useKey.getBoundKeyTranslationKey()), false);
+                player.getInventory().selectedSlot = InventoryUtil.beforeSlot;
+                KindaTweaksClient.mc.getNetworkHandler().sendPacket(new UpdateSelectedSlotC2SPacket(player.getInventory().selectedSlot));
+                flag = false;
             }
         }
     }
--- src/main/java/com/pugur/kindatweaks/util/InventoryUtil.java
+++ src/main/java/com/pugur/kindatweaks/util/InventoryUtil.java
@@ -1,23 +1,21 @@
 package com.pugur.kindatweaks.util;
 
-import com.pugur.kindatweaks.KindaTweaks;
 import com.pugur.kindatweaks.KindaTweaksClient;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.network.packet.c2s.play.UpdateSelectedSlotC2SPacket;
 import net.minecraft.screen.ScreenHandler;
-import net.minecraft.screen.slot.Slot;
 import net.minecraft.screen.slot.SlotActionType;
 
 import java.util.Objects;
 
 public class InventoryUtil {
+    public static int beforeSlot;
     private static int FOOD_SWITCH_SLOT;
     public static void setFoodSwitchSlot(int slot) {
         if (slot > 0 && slot <= 9) {
             FOOD_SWITCH_SLOT = slot - 1;
-            KindaTweaks.logger.info("a");
         }
     }
 
@@ -26,7 +24,8 @@ public class InventoryUtil {
         if (player != null && player.getWorld() != null) {
             PlayerInventory inventory = player.getInventory();
             ScreenHandler container = player.playerScreenHandler;
-            if (slot >= 0 && slot - 36 != inventory.selectedSlot && player.currentScreenHandler == player.playerScreenHandler) {
+            if (slot >= 0 && slot!= inventory.selectedSlot && player.currentScreenHandler == player.playerScreenHandler) {
+                beforeSlot = inventory.selectedSlot;
                 if (PlayerInventory.isValidHotbarIndex(slot)) {
                     inventory.selectedSlot = slot;
                     mc.getNetworkHandler().sendPacket(new UpdateSelectedSlotC2SPacket(inventory.selectedSlot));
@@ -35,9 +34,7 @@ public class InventoryUtil {
                         inventory.selectedSlot = FOOD_SWITCH_SLOT;
                         Objects.requireNonNull(KindaTweaksClient.mc.getNetworkHandler()).sendPacket(new UpdateSelectedSlotC2SPacket(player.getInventory().selectedSlot));
                     }
-                    if (slot != inventory.selectedSlot) {
-                        mc.interactionManager.clickSlot(container.syncId, slot, FOOD_SWITCH_SLOT, SlotActionType.SWAP, mc.player);
-                    }
+                    mc.interactionManager.clickSlot(container.syncId, slot, FOOD_SWITCH_SLOT, SlotActionType.SWAP, mc.player);
                 }
             }
         }
